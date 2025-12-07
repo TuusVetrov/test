09d66493366b3d1c2830ff660459ab311695b322
Никита Клёмин <truevetrov1337@gmail.com>
2025-12-07 22:04:56 +0300

    feat(api): добавлен контроллер для работы с услугами

--- feature/addeditroutine/src/main/java/com/rendox/routinetracker/addeditroutine/setgoal/SetGoalPage.kt
+++ feature/addeditroutine/src/main/java/com/rendox/routinetracker/addeditroutine/setgoal/SetGoalPage.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.rendox.routinetracker.addeditroutine.AddHabitDestinationTopAppBar
 import com.rendox.routinetracker.addeditroutine.navigation.AddRoutineDestination
@@ -67,4 +68,10 @@ fun SetGoalPage(
 //            },
 //        )
     }
+}
+
+@Preview
+@Composable
+private fun Preview() {
+    // TODO:: make feat
 }
